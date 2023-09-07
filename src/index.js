d7a3e11e6037fe5c4b350271db19706c33b24e4b
Gianfranco castro <castropadillagian@gmail.com>
2023-09-07 15:33:13 -0500

    Carpeta public con vistas de home y compra exitosa agregagas, así como el rediccionamiento luego de hacer la compra efectiva

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import paymentRoutes from "./routes/payment.routes.js";
 import { PORT } from "./config.js";
+import path from "path";
 
 const app = express(); //Creaamos una instancia de express para poder usar sus metodos y propiedades
 
 app.use(express.json()); //Para que express pueda entender los datos que le enviamos en formato json
 
 app.use(paymentRoutes)//Usamos el router que creamos en el archivo payment.routes.js
+
+app.use(express.static(path.resolve('src/public'))) //Para que express pueda servir archivos estaticos (html, css, js, imagenes, etc
 
 app.listen(PORT, () => console.log("Server running on port", PORT)); //Levantamos el servidor en el puerto 3000
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -3,8 +3,10 @@ import { crearSesionDePago } from "../controllers/payment.controller.js";
 
 const router = Router(); //Creamos una instancia de router para poder usar sus metodos y 
 
-router.get("/crear-orden-compra", crearSesionDePago); //Creamos una ruta para crear una orden de compra
-router.get("/compra_exitosa", (req, res) => (res.send("Pago exitoso"))); //Creamos una ruta para cuando el pago sea exitoso
-router.get("/compra_fallida", (req, res) => (res.send("Pago fallido"))); //Creamos una ruta para cuando el pago sea fallido
+router.post("/crear-orden-compra", crearSesionDePago); //Creamos una ruta para crear una orden de compra
+
+router.get("/compra_exitosa", (req, res) => (res.redirect('/compraExitosa.html'))); //Creamos una ruta para cuando el pago sea exitoso
+
+router.get("/compra_fallida", (req, res) => (res.redirect('/'))); //Creamos una ruta para cuando el pago sea fallido
 
 export default router; //Exportamos el router para poder usarlo en otros archivos
