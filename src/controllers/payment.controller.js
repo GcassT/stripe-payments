b2ef5d1c8239fbbf36d1950a59eff40b514ecf0a
Gianfranco castro <castropadillagian@gmail.com>
2023-09-07 14:38:38 -0500

    Correcciones en el controlador para el correcto funcionamiento del pago

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -6,35 +6,35 @@ const stripe = new Stripe('sk_test_51Nn1oCLvwEnXJnI5PG6P8OF8CXbq6UFCaWASHtDMsvW4
 
 export const crearSesionDePago = async (req, res) => {
    const sesionPago = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'], //Metodo de pago
         line_items: [
             {
-                dato_precio: {
-                    divisa: 'usd',
-                    precio_unidad: 20000,
-                dato_producto: {
-                    nomnbre: 'Torre de computadora',
-                    descripcion: 'Torre de computadora gamer con luces RGB',
+            price_data: {
+                product_data: {
+                    name: 'Torre de computadora',
+                    description: 'Torre de computadora gamer con luces RGB',
                 },
-                cantidad: 1,
+                currency: 'usd',
+                unit_amount: 20000, //200.00 dlls
             },
-                dato_precio: {
-                    divisa: 'usd',
-                    precio_unidad: 3000,
-                dato_producto: {
-                    nomnbre: 'Monitor',
-                    descripcion: 'Monitor de 24 pulgadas',
-                    
-                },
-                cantidad: 1,
-            },
+            quantity: 1, //Cantidad de productos
         },
-    ], 
+        {
+            price_data: {
+                product_data: {
+                    name: 'Monitor',
+                    description: 'Monitor de 24 pulgadas',
+                },
+                currency: 'usd',
+                unit_amount: 3000, //30.00 dlls
+            },
+            quantity: 2, //Cantidad de productos
+        },
+        ], 
 
         mode: 'payment', //Modo de pago
-        compra_exitosa_url: 'http://localhost:3000/compra_exitosa', //URL de redireccionamiento cuando el pago sea exitoso
-        compra_fallida_url: 'http://localhost:3000/compra_fallida', //URL de redireccionamiento cuando el pago sea fallido
+        success_url: 'http://localhost:3000/compra_exitosa', //URL de redireccionamiento cuando el pago sea exitoso
+        cancel_url: 'http://localhost:3000/compra_fallida', //URL de redireccionamiento cuando el pago sea fallido
     })
 
-    return res.json(sesionPago)
+    return res.json(sesionPago);
 }
